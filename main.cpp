f845e0926458e3c66e0db548e21028dd2a52903f
Egorov16 <erovskiy12@gmail.com>
2024-06-22 19:51:30 +0300

    shapeFunction

--- main.cpp
+++ main.cpp
@@ -6,6 +6,58 @@
 #include <set>
 #include <vector>
 
+
+// Функция формы
+float shapeFunction(float localCoord, float xi) {
+  // Линейная функция формы
+  return 0.5 * (1.0 + xi * localCoord);
+}
+
+// Функция, которая вычисляет значение в заданных локальных координатах
+float calculateValue(float localX, float localY, float localZ,
+                     const std::vector<float> &nodalValues) {
+  // Проверяем размер массива узловых значений
+  if (nodalValues.size() != 8) {
+    std::cerr << "Ошибка: Массив узловых значений должен иметь 8 элементов!"
+              << std::endl;
+    return 0.0;
+  }
+
+  // Вычисляем значения функций формы
+  float N1 = shapeFunction(localX, -1.0) * shapeFunction(localY, -1.0) *
+             shapeFunction(localZ, -1.0);
+  std::cout << "N1 = " << N1 << std::endl;
+  float N2 = shapeFunction(localX, 1.0) * shapeFunction(localY, -1.0) *
+             shapeFunction(localZ, -1.0);
+  std::cout << "N2 = " << N2 << std::endl;
+  float N3 = shapeFunction(localX, 1.0) * shapeFunction(localY, 1.0) *
+             shapeFunction(localZ, -1.0);
+  std::cout << "N3 = " << N3 << std::endl;
+  float N4 = shapeFunction(localX, -1.0) * shapeFunction(localY, 1.0) *
+             shapeFunction(localZ, -1.0);
+  std::cout << "N4 = " << N4 << std::endl;
+  float N5 = shapeFunction(localX, -1.0) * shapeFunction(localY, -1.0) *
+             shapeFunction(localZ, 1.0);
+  std::cout << "N5 = " << N5 << std::endl;
+  float N6 = shapeFunction(localX, 1.0) * shapeFunction(localY, -1.0) *
+             shapeFunction(localZ, 1.0);
+  std::cout << "N6 = " << N6 << std::endl;
+  float N7 = shapeFunction(localX, 1.0) * shapeFunction(localY, 1.0) *
+             shapeFunction(localZ, 1.0);
+  std::cout << "N7 = " << N7 << std::endl;
+  float N8 = shapeFunction(localX, -1.0) * shapeFunction(localY, 1.0) *
+             shapeFunction(localZ, 1.0);
+  std::cout << "N8 = " << N8 << std::endl;
+
+  // Вычисляем значение в точке
+  float value = N1 * nodalValues[0] + N2 * nodalValues[1] +
+                N3 * nodalValues[2] + N4 * nodalValues[3] +
+                N5 * nodalValues[4] + N6 * nodalValues[5] +
+                N7 * nodalValues[6] + N8 * nodalValues[7];
+
+  return value;
+}
+
 int main() {
   setlocale(LC_ALL, "rus");
 
@@ -13,10 +65,10 @@ int main() {
   struct Node {
     float x, y, z;
   };
+
   std::vector<Node> crd;
   {
-    std::ifstream in("crd.sba",
-                     std::ifstream::binary);
+    std::ifstream in("crd.sba", std::ifstream::binary);
     if (!in.good())
       throw std::runtime_error("Crd");
     int numNodes, kort;
@@ -67,8 +119,7 @@ int main() {
 
   // ind
   {
-    std::ifstream in("ind.sba",
-                     std::ifstream::binary);
+    std::ifstream in("ind.sba", std::ifstream::binary);
     if (!in.good())
       throw std::runtime_error("Ind");
     int numTypes;
@@ -89,7 +140,8 @@ int main() {
         ind.resize(numIndexes / t.second);
         in.read((char *)ind.data(), sizeof(int) * ind.size());
         elems.push_back(std::make_pair(
-            t.first, ind)); // Заполняем вектор elems парами Тип элемента - список узлов
+            t.first, ind)); // Заполняем вектор elems парами номер КЭ - список
+                            // узлов, которые в него включены
       }
     }
   }
@@ -114,6 +166,17 @@ int main() {
     }
     std::cout << std::endl;
   }
+
+  // Пример использования функции calculateValue
+  std::vector<float> nodalValues = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0};
+  float localX = 0.5;
+  float localY = 0.25;
+  float localZ = -0.75;
+
+  float value = calculateValue(localX, localY, localZ, nodalValues);
+
+  std::cout << "Значение в точке (" << localX << ", " << localY << ", "
+            << localZ << "): " << value << std::endl;
 
   return 0;
 }
